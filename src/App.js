2e74a44ef2ae90c91c7ed387765e11f99de50b38
anwarmontasir <anwarmontasir@gmail.com>
2021-09-23 20:06:51 -0700

    Routes and Links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/newMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
   return (
     <div>
-      Hello world
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
